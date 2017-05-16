047367b183053a55f3a597e3acc95b0c40b20729
Ezequiel Muns <ezeq.au@gmail.com>
2017-05-16 20:50:35 +0200

    Interactivity via css and js.
    Circle segments highlight on hover
    Some have js actions attached
    Importing svg directly into home as a template

---
+++ public/js/circle.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('#CoV #art-main').click(function () {
+        window.alert('You clicked the thing!');
+    });
+    $('#CoV #land-sounds').click(function () {
+        window.alert('You clicked land sounds!');
+    });
+});
+
