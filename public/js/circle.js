6be50cf3d55a6a3606e63d047c9a1088799c8155
Ezequiel Muns <ezeq.au@gmail.com>
2017-05-21 18:41:38 +0200

    New cleaned SVG with classes and links

--- public/js/circle.js
+++ public/js/circle.js
@@ -1,9 +1,4 @@
 $(document).ready(function () {
-    $('#CoV #art-main').click(function () {
-        window.alert('You clicked the thing!');
-    });
-    $('#CoV #land-sounds').click(function () {
-        window.alert('You clicked land sounds!');
-    });
+
 });
 
